549d51cac8c417aa1e92a41807c3f6591e2ddeb3
Xeptao <xeptaowastaken@gmail.com>
2021-06-14 21:57:10 +0300

    fix: fix url validating algorithm

--- src/core/useQuery/useQuery.ts
+++ src/core/useQuery/useQuery.ts
@@ -5,8 +5,10 @@ import axios from "axios";
 async function useQuery<ReturnValue = unknown>(
   url: string
 ): Promise<ReturnValue> {
-  if (!url.includes("https://") ?? !url.includes("http://"))
-    throw 'The property "url" is not a valid url.';
+  // provide a regex to find if url is valid or not - even accepts localhost:port
+  const urlRegex = new RegExp("/^(?:w+:)?//([^s.]+.S{2}|localhost[:?d]*)S*$/");
+
+  if (urlRegex.test(url)) throw 'The property "url" is not a valid url.';
 
   const { data } = await axios.get(url);
 
