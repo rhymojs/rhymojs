9cd215dddccfca4e1394606e6b280a423b2f068a
Xeptao <xeptao@xeptao.com>
2021-06-28 22:24:21 +0300

    refactor: remove bad tests

--- src/core/tests/toCapitalizeCase.test.ts
+++
@@ -1,7 +0,0 @@
-import toCapitalizeCase from "../toCapitalizeCase";
-
-describe("toTitleCase", () => {
-  it("works properly", () => {
-    expect(toCapitalizeCase("hello world")).toEqual("Hello World");
-  });
-});
