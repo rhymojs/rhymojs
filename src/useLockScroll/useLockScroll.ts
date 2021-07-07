f1ed965198fab5248b3f19b5139f6382ad410f38
Xeptao <xeptaowastaken@gmail.com>
2021-07-07 12:24:11 +0300

    feat: useLockScroll
    Prevents users from scrolling if the state is true.

---
+++ src/useLockScroll/useLockScroll.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
+/**
+ * Prevents users from scrolling if the state is true.
+ * @version v1.4.1
+ * @see https://rhymojs.com/docs/useLockScroll/
+ */
+const useLockScroll = (): [boolean, Dispatch<SetStateAction<boolean>>] => {
+  const [lockScroll, setLockScroll] = useState(false);
+  const html = document.querySelector("html") as HTMLHtmlElement;
+
+  useEffect(() => {
+    html.style.overflow = lockScroll ? "hidden" : "visible";
+  }, [lockScroll]);
+
+  return [lockScroll, setLockScroll];
+};
+
+export default useLockScroll;
