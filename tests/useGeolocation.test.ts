6c63015904685b57d0e16eb2c9799e81a2b1dc4e
Xeptao <xeptaowastaken@gmail.com>
2021-07-09 15:00:57 +0300

    test: write tests for `useGeolocation`

---
+++ tests/useGeolocation.test.ts
@@ -0,0 +1,15 @@
+import { useGeolocation as getGeolocation } from "../lib";
+
+describe("getGeolocation", () => {
+  it("properly returns data", async () => {
+    const geolocation = await getGeolocation();
+
+    expect(typeof geolocation).toBe("string");
+  });
+
+  it("returns an ip", async () => {
+    const geolocation = await getGeolocation();
+
+    expect(geolocation.length).toBeGreaterThanOrEqual(14);
+  });
+});
