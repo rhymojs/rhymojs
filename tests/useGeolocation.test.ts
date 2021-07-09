699750c165a6acbda9417c2dbe880d13511062e4
Xeptao <xeptao@xeptao.com>
2021-07-09 23:17:03 +0300

    feat: ip validator (#52)
    Validate IP adresses with the help of Regular Expressions.

--- tests/useGeolocation.test.ts
+++ tests/useGeolocation.test.ts
@@ -9,7 +9,9 @@ describe("getGeolocation", () => {
 
   it("returns an ip", async () => {
     const geolocation = await getGeolocation();
-
-    expect(geolocation.length).toBeGreaterThanOrEqual(13);
+    const IpRegex = new RegExp("/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/");
+    
+    
+    expect(IpRegex.test(geolocation)).toEqual(true);
   });
 });
